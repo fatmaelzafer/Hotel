b57f2e770dd761f24dd7f09e05ca1cffa492709a
fatmaelzafer <fatmaelzafer0@gmail.com>
2026-09-20 01:57:24 +0300

    sec commit2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,12 +5,12 @@ export const serverRoutes: ServerRoute[] = [
   { path: 'signup', renderMode: RenderMode.Server },
   { path: 'booking/:id', renderMode: RenderMode.Server },
   { path: 'guestview', renderMode: RenderMode.Server },
+  { path: 'guestview/home', renderMode: RenderMode.Server },
+  { path: 'guestview/rooms', renderMode: RenderMode.Server },
+  { path: 'guestview/roomsearch/:checkOut/:checkIn/:guests', renderMode: RenderMode.Server },
   { path: 'home', renderMode: RenderMode.Server },
   { path: 'rooms', renderMode: RenderMode.Server },
-  {
-    path: 'guestview/roomsearch/:checkOut/:checkIn/:guests', 
-    renderMode: RenderMode.Server,
-  },
+  { path: 'roomsearch/:checkOut/:checkIn/:guests', renderMode: RenderMode.Server },
   {
     path: '**',
     renderMode: RenderMode.Prerender,
